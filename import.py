9a99a9020c5dbe9b49255a37bba96d2a216dcbc0
hoideptrai123 <toilahoi007@gmail.com>
2019-04-19 23:25:50 +0700

    complete

--- import.py
+++ import.py
@@ -10,15 +10,19 @@ import time
 # current date and time
 # timestamp = calendar.timegm(time.gmtime())
 template = []
+
+
 def get_template_line():
      # Read data that needs processing from CSV files
-   
+
     with open('product_template.csv', newline='') as f:
         reader = csv.DictReader(f)
         for row in reader:
             template.append(row)
 
-    return template[0]
+    temp1 = copy.deepcopy(template[0])
+    del temp1['Keyword']
+    return temp1
 
 
 template_line = get_template_line()
@@ -26,13 +30,12 @@ variable_data = copy.deepcopy(template_line)
 variation_data = copy.deepcopy(template_line)
 fileCSVs = glob.glob("list_csv_in/*.csv")
 fieldnames = template_line.keys()
-datas=[]
+datas = []
 
 for file in fileCSVs:
-    datas=[]
+    datas = []
     with open('list_csv_in/'+file[12:]) as f:
         reader = csv.DictReader(f)
-
         for row in reader:
             datas.append(row)
 
@@ -40,35 +43,34 @@ for file in fileCSVs:
         writer = csv.DictWriter(f, fieldnames=fieldnames)
         writer.writeheader()
         isVariation = 0
-        for row in datas :
-            name=''
+        name = ''
+        for row in datas:
             timestamp = calendar.timegm(time.gmtime())
-            if row['Type']=='variable' :
-                for temp in template :
-                    if temp['Type']=='variable' : 
-                        isVariation=0
-                        keys=temp['Keyword'].split('/')
-                        if  keys[0].lower().strip() in row['Name'].lower().strip() or (len(keys)>0 and keys[len(keys)-1].lower().strip() in row['Name'].lower().strip() ) : 
-                            isVariation=1
-                            temp['Description']=row['Name']+ row['Description'][8:]
-                            temp['Short description']=row['Name']
-                            temp['Images']=row['Images']
-                            temp['Name']=row['Name']
-                            temp['SKU']=timestamp
-                            name=row['Name']
-                            temp['Name']=row['Name'] 
-                            writer.writerow(temp)
+            if row['Type'] == 'variable':
+                for temp in template:
+                    variable_data=copy.deepcopy(temp)
+                    if variable_data['Type'] == 'variable':
+                        if name == row['Name']:
+                            break
+                        isVariation = 0
+                        keys = temp['Keyword'].split('/')
+                        if (keys[0].lower() in row['Name'].lower() or (len(keys) > 0 and keys[len(keys)-1].lower() in row['Name'].lower())) and len(variable_data['Description'])>0:
+                            isVariation = 1
+                            variable_data['Description'] =  row['Name']+ ' ' +variable_data['Description'][8:]
+                            variable_data['Short description'] = row['Name']
+                            variable_data['Images'] = row['Images']
+                            variable_data['Name'] = row['Name']
+                            variable_data['SKU'] = timestamp
+                            name = row['Name']
+                            variable_data['Name'] = row['Name']
+                            a= copy.deepcopy(variable_data)
+                            del a['Keyword']
+                            writer.writerow(a)
                     elif isVariation == 1:
-                        temp['Name']=name 
-                        temp['Parent']=timestamp 
-                        writer.writerow(temp)
-
-                    
-            
-                            
-                
-
-        
-
-
+                        variable_data['Name'] = name
+                        variable_data['Parent'] = timestamp
+                        a= copy.deepcopy(variable_data)
+                        del a['Keyword']
+                        writer.writerow(a)
+    
 
