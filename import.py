f0017fc5624cfa6c0b154ed8350a5f201a49cdc8
vuhoi <toilahoi007@gmail.com>
2019-04-19 14:28:51 +0700

    complete

--- import.py
+++ import.py
@@ -39,25 +39,31 @@ for file in fileCSVs:
     with open('list_csv_out\\'+file[12:], 'w', newline='\n') as f:
         writer = csv.DictWriter(f, fieldnames=fieldnames)
         writer.writeheader()
-        for temp in template :
+        isVariation = 0
+        for row in datas :
             name=''
             timestamp = calendar.timegm(time.gmtime())
-            if temp['Type']=='variable' :
-                for row in datas :
-                    if row['Type']=='variable' : 
+            if row['Type']=='variable' :
+                for temp in template :
+                    if temp['Type']=='variable' : 
+                        isVariation=0
                         keys=temp['Keyword'].split('/')
                         if  keys[0].lower().strip() in row['Name'].lower().strip() or (len(keys)>0 and keys[len(keys)-1].lower().strip() in row['Name'].lower().strip() ) : 
+                            isVariation=1
                             temp['Description']=row['Name']+ temp['Description'][8:]
                             temp['Short description']=row['Name']
                             temp['Images']=row['Images']
                             temp['Name']=row['Name']
                             temp['SKU']=timestamp
                             name=row['Name']
-                            break
-            else :
-                    temp['Parent']=timestamp 
-            temp['Name']=name 
-            writer.writerow(temp)
+                            temp['Name']=row['Name'] 
+                            writer.writerow(temp)
+                    elif isVariation == 1:
+                        temp['Name']=name 
+                        temp['Parent']=timestamp 
+                        writer.writerow(temp)
+
+                    
             
                             
                 
